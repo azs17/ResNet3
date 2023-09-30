1721029a5374adcc2db2c79a2329aa93cdcab246
azs17 <azs17@miami.edu>
2023-09-30 16:13:53 -0400

    plotting draft abcdefg

--- ResNet3.py
+++ ResNet3.py
@@ -385,8 +385,10 @@ with torch.no_grad():
     matplotlib.use('Agg')
     pred_np = predicted_testlabel_list
     for i in range(50):
+        pred = int(predicted_testlabel_list[0][i].item())
+
         print('label ',label_test_np[i]) 
-        print('predicted ',predicted_testlabel_list[0][i].item())
+        print('predicted ',pred)
         print('accuracy', acc_test_list[0])
         # print('image no',i)
         # print('a',acc_test_list[0])
@@ -395,7 +397,7 @@ with torch.no_grad():
         img = test_ds[i,:,:]
         print('i', i, 'img', img.shape)
         plt.figure(figno)
-        plt.title("test %d label %d pred %d accur %.2f" % (i, label_test_np[i], predicted_testlabel_list[0][i].item(), acc_test_list[0]))
+        plt.title("test %d label %d pred %d accur %.2f" % (i, label_test_np[i], pred, acc_test_list[0]))
         plt.imshow(X=img, cmap='gray', vmin=0, vmax=255)
         #plt.show()
         plt.savefig('%s/test/%05d.png' % (odir, i))
