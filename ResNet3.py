ccd586d74000ff73d51e5c582b1f687489fd4c2c
azs17 <azs17@miami.edu>
2023-09-30 16:04:19 -0400

    plotting draft abc

--- ResNet3.py
+++ ResNet3.py
@@ -30,7 +30,7 @@ mkdir(odir)
 #---
 dataset = torchvision.datasets.CIFAR10(root='./content/', download=True, train = True, transform=None)
 testset = torchvision.datasets.CIFAR10(root='./content/', train=False, transform=None)
-
+label_test_np  = np.array(testset.targets)
 
 dataset_tens = torch.tensor(dataset.data, requires_grad=False, device=my_device, dtype=torch.float32)
 dataset_labels_tens = torch.tensor(dataset.targets, requires_grad=False, device=my_device, dtype=torch.float32)
@@ -51,7 +51,7 @@ val_ds = val_ds.permute(0,3,1,2)
 test_ds = test_ds.permute(0,3,1,2)
 
 test_ds_np      = test_ds.data
-label_test_np  = np.array(testset_labels_tens.cpu())
+
 
 print(train_ds.shape)
 print(train_ds_labels.shape)
